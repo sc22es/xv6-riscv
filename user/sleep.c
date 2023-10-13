625f6f3c448173cec790e661e211acabf8f5a65f
Ellis Stonehouse <sc22es@leeds.ac.uk>
2023-10-13 14:19:22 +0100

    sleep arg

--- user/sleep.c
+++ user/sleep.c
@@ -5,5 +5,13 @@
 
 int main(int argc, char *argv[]) {
     printf("fj");
+
+    // only allows 1 arg
+    if (argc == 1) {
+        printf("ERROR: Sleep only accepts 1 argument\n");
+        exit(0);
+    }
+
+    
     exit(0);
 };
